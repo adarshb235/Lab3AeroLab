b38d5b2d73132986aa7160ad955a41d3733b2eb0
PLarson70 <pala2314@colorado.edu>
2025-11-15 01:50:00 -0700

    Add files via upload
    THIS IS A WORK IN PROGRESS

---
+++ Lab3_Part2.m
@@ -0,0 +1,70 @@
+%% Lab 3 Part 2
+clc;
+close all;
+clear;
+
+% Part 2 Task 1 - Prandtl Lifting Line Code
+
+%wing characteristics
+b= 100; %span [feet]
+a0_t = 2*pi; %cross-sectional lift slope at the tips [radians]
+a0_r = 2*pi; % cross-section lift slope at the root [radians]
+c_t = 8; % chord at tips [ft]
+c_r = 10; % chord at root [ft]
+aero_t = 0; % zero-lift AoA at tips [radians]
+aero_r=0; % zero-lift AoA at root [radians]
+geo_t = 5*pi/180; % geometric AoA (geometric twist + alpha) at tips [radians]
+geo_r = 7*pi/180; % geometric AoA (geometric twist + alpha) at root [radians]
+
+N = 5; % # of odd terms for circulation
+
+
+[e,c_L,c_Di,A_matrix] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N);
+
+
+function [e,c_L,c_Di,A_matrix] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
+    theta = (1:N)' * pi / (2*N);
+    c = c_r + (c_t-c_r) * cos(theta);
+    a0 = a0_r + (a0_t - a0_r)*cos(theta);
+    alphaL0 = aero_r + (aero_t - aero_r) * cos(theta);
+    alpha_geo = geo_r +(geo_t - geo_r) *cos(theta);
+    b_i=(alpha_geo - alphaL0);
+    Mij = zeros(N);
+    for i = 1:N
+        for j = 1:N
+            Mij(i,j) = ( (4*b)./ (a0(i).*c(i)) ) .* sin(((2.*j) - 1).*theta(i)) + ((2*j)-1)*((sin(((2.*j) - 1).*theta(i)))./sin(theta(i)));
+        end
+    end
+    A_matrix = Mij\b_i;
+    
+    S = b*((c_r +c_t)/2);
+    AR = b^2/S;
+    %coefficent of lift
+    c_L = A_matrix(1)*pi*AR;
+
+    %could be an error from here down because c_Di and e don't match the
+    %values in the annocment
+    C_Di_total = 0;
+    for j=1:N
+        delta(j)= C_Di_total;
+        C_Di_total = C_Di_total + (2*j-1)*((A_matrix(j)/A_matrix(1))^2);
+       
+    end
+    %e
+    e = (1 + C_Di_total)^(-1);
+    %coefficent of drag
+    c_Di = (c_L^2)/(pi*AR*e);
+
+    %taper = linspace(0,(c_t/c_r),length(delta));
+    % figure()
+    % hold on;
+    % plot(taper, delta,'k');
+    % xlabel('Taper ratio c_t/c_r');
+    % ylabel('delta');
+
+end
+
+    % 
+    % for i=1:length(y_0)
+    %     sec_lift = rho_inf*v_inf *circ(y_0);
+    % end
