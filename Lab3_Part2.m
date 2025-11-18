e474020df4a7b11fb9ea82a42f535d53b8fa85b6
Adarsh Boddeda <46360206+adarshb235@users.noreply.github.com>
2025-11-18 09:24:23 -0700

    Revise wing characteristics and PLLT calculations (Working Task 1)
    Updated wing characteristics and calculations in PLLT function.

--- Lab3_Part2.m
+++ Lab3_Part2.m
@@ -7,12 +7,12 @@ clear;
 
 %wing characteristics
 b= 100; %span [feet]
-a0_t = 2*pi; %cross-sectional lift slope at the tips [radians]
-a0_r = 2*pi; % cross-section lift slope at the root [radians]
+a0_t = 6.3; %cross-sectional lift slope at the tips [radians]
+a0_r = 6.5; % cross-section lift slope at the root [radians]
 c_t = 8; % chord at tips [ft]
 c_r = 10; % chord at root [ft]
 aero_t = 0; % zero-lift AoA at tips [radians]
-aero_r=0; % zero-lift AoA at root [radians]
+aero_r = -2*pi/180; % zero-lift AoA at root [radians]
 geo_t = 5*pi/180; % geometric AoA (geometric twist + alpha) at tips [radians]
 geo_r = 7*pi/180; % geometric AoA (geometric twist + alpha) at root [radians]
 
@@ -24,15 +24,18 @@ N = 5; % # of odd terms for circulation
 
 function [e,c_L,c_Di,A_matrix] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,geo_r,N)
     theta = (1:N)' * pi / (2*N);
-    c = c_r + (c_t-c_r) * cos(theta);
-    a0 = a0_r + (a0_t - a0_r)*cos(theta);
-    alphaL0 = aero_r + (aero_t - aero_r) * cos(theta);
-    alpha_geo = geo_r +(geo_t - geo_r) *cos(theta);
-    b_i=(alpha_geo - alphaL0);
+    c = c_r + (c_t-c_r) .* cos(theta);
+    a0 = a0_r + (a0_t - a0_r) .* cos(theta);
+    alphaL0 = aero_r + (aero_t - aero_r) .* cos(theta);
+    alpha_geo = geo_r + (geo_t - geo_r) .* cos(theta);
+    b_i = (alpha_geo - alphaL0);
     Mij = zeros(N);
     for i = 1:N
+        ac_Theta = theta(i);
+        mu = 4 * b / (a0(i) * c(i));
         for j = 1:N
-            Mij(i,j) = ( (4*b)./ (a0(i).*c(i)) ) .* sin(((2.*j) - 1).*theta(i)) + ((2*j)-1)*((sin(((2.*j) - 1).*theta(i)))./sin(theta(i)));
+            n = 2 * j - 1;
+            Mij(i,j) = (mu + (n / sin(ac_Theta))) * sin(n * ac_Theta);
         end
     end
     A_matrix = Mij\b_i;
@@ -42,16 +45,16 @@ function [e,c_L,c_Di,A_matrix] = PLLT(b,a0_t,a0_r,c_t,c_r,aero_t,aero_r,geo_t,ge
     %coefficent of lift
     c_L = A_matrix(1)*pi*AR;
 
-    %could be an error from here down because c_Di and e don't match the
-    %values in the annocment
     C_Di_total = 0;
     for j=1:N
-        delta(j)= C_Di_total;
-        C_Di_total = C_Di_total + (2*j-1)*((A_matrix(j)/A_matrix(1))^2);
-       
+        n = 2 * j - 1;
+        C_Di_total = C_Di_total + n * (A_matrix(j).^2);
     end
+
+    c_Di = pi * AR * C_Di_total;
+
     %e
-    e = (1 + C_Di_total)^(-1);
+    e = A_matrix(1).^2 ./C_Di_total;
     %coefficent of drag
     c_Di = (c_L^2)/(pi*AR*e);
 
